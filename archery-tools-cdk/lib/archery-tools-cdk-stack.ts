8c6959a63a92bb964aefc4a873f8e366aa3d78c6
Yukio Usuzumi <anohigisavay@gmail.com>
2024-06-04 22:18:23 -0700

    Integrate API Gateway into CDK

--- archery-tools-cdk/lib/archery-tools-cdk-stack.ts
+++ archery-tools-cdk/lib/archery-tools-cdk-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import path = require('path');
 import { cdkRootDir } from './env';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 function getLambdaZip(binName: string): string {
@@ -24,12 +25,21 @@ export class ArcheryToolsCdkStack extends cdk.Stack {
     });
 
     // The Scoresheet RESTful resource
-    new lambda.Function(this, 'ScoreSheetFunction', {
+    const scoreSheetFunction = new lambda.Function(this, 'ScoreSheetFunction', {
       architecture: lambda.Architecture.ARM_64,
       runtime: lambda.Runtime.PROVIDED_AL2023,
       handler: 'bootstrap',
       code: lambda.Code.fromAsset(getLambdaZip('scoresheet')),
       tracing: lambda.Tracing.ACTIVE
-    })
+    });
+
+    // API Gateway
+    new apigateway.LambdaRestApi(this, 'ScoreSheet', {
+      handler: scoreSheetFunction,
+      integrationOptions: {
+        proxy: true,
+        allowTestInvoke: true
+      }
+    });
   }
 }
