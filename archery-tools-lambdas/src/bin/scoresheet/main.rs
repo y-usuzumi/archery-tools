ded8bbd9c03e40429082766c0debe5ad6d7113d4
Yukio Usuzumi <anohigisavay@gmail.com>
2024-06-28 18:48:38 -0700

    Use lambda-events and ApiGatewayProxy* models so that it works with Lambda proxy integration

--- archery-tools-lambdas/src/bin/scoresheet/main.rs
+++ archery-tools-lambdas/src/bin/scoresheet/main.rs
@@ -1,3 +1,7 @@
+use aws_lambda_events::{
+    apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse},
+    http::HeaderMap,
+};
 use lambda_runtime::{service_fn, tracing, Error, LambdaEvent};
 use serde::{Deserialize, Serialize};
 
@@ -12,12 +16,34 @@ struct Distance {
     value: f64,
 }
 
-async fn func(event: LambdaEvent<Point>) -> Result<Distance, Error> {
+// async fn func(event: LambdaEvent<Point>) -> Result<Distance, Error> {
+//     let (point, _ctx) = event.into_parts();
+
+//     let result = f64::sqrt(point.x as f64 * point.x as f64 + point.y as f64 * point.y as f64);
+
+//     Ok(Distance { value: result })
+// }
+
+async fn func(
+    event: LambdaEvent<ApiGatewayProxyRequest>,
+) -> Result<ApiGatewayProxyResponse, Error> {
     let (point, _ctx) = event.into_parts();
+
+    let point: Point = serde_json::from_str(&point.body.unwrap())?;
 
     let result = f64::sqrt(point.x as f64 * point.x as f64 + point.y as f64 * point.y as f64);
 
-    Ok(Distance { value: result })
+    let mut headers = HeaderMap::new();
+    headers.insert("content-type", "text/html".parse().unwrap());
+    let resp = ApiGatewayProxyResponse {
+        status_code: 200,
+        multi_value_headers: headers.clone(),
+        is_base64_encoded: false,
+        body: Some((serde_json::to_string(&Distance { value: result })?).into()),
+        headers,
+    };
+
+    Ok(resp)
 }
 
 #[tokio::main]
